3b966d52ca71fc149204d19e5a89ca67b7a28d67
nada <hammadinada6@gmail.com>
2024-04-25 15:10:56 +0200

    Import folder [IoTminiproject]

---
+++ WiFi_HTTP_Server_file/Debug/Application/WIFI/es_wifi.d
@@ -0,0 +1,5 @@
+Application/WIFI/es_wifi.o: \
+ E:/STM32Cube_FW_L4_V1.18.0/Projects/B-L475E-IOT01A/Applications/WiFi/Common/Src/es_wifi.c \
+ ../../../Common/Inc/es_wifi.h ../../Inc/es_wifi_conf.h
+../../../Common/Inc/es_wifi.h:
+../../Inc/es_wifi_conf.h:
---
+++ WiFi_HTTP_Server_file/Debug/Drivers/BSP/hts221.d
@@ -0,0 +1,8 @@
+Drivers/BSP/hts221.o: \
+ E:/STM32Cube_FW_L4_V1.18.0/Drivers/BSP/Components/hts221/hts221.c \
+ E:/STM32Cube_FW_L4_V1.18.0/Drivers/BSP/Components/hts221/hts221.h \
+ E:/STM32Cube_FW_L4_V1.18.0/Drivers/BSP/Components/hts221/../Common/hsensor.h \
+ E:/STM32Cube_FW_L4_V1.18.0/Drivers/BSP/Components/hts221/../Common/tsensor.h
+E:/STM32Cube_FW_L4_V1.18.0/Drivers/BSP/Components/hts221/hts221.h:
+E:/STM32Cube_FW_L4_V1.18.0/Drivers/BSP/Components/hts221/../Common/hsensor.h:
+E:/STM32Cube_FW_L4_V1.18.0/Drivers/BSP/Components/hts221/../Common/tsensor.h:
